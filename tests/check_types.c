c63c8c61d966b1c375d1dee88248c4fea408d158
b17v134 <b17v134@users.noreply.github.com>
2022-10-15 12:32:01 +0300

    Added test int_to_dns_type

--- tests/check_types.c
+++ tests/check_types.c
@@ -27,15 +27,41 @@ START_TEST(test_dns_type_to_int)
 }
 END_TEST
 
+
+START_TEST(test_int_to_dns_type)
+{
+	struct test_data
+	{
+		uint16_t type;
+		char* result;
+	};
+
+	struct test_data data[] = {
+		{9999, "ERROR"},
+		{DNS_TYPE_ERROR, "ERROR"},
+		{DNS_TYPE_A, "A"},
+		{DNS_TYPE_NS, "NS"},
+		{DNS_TYPE_AAAA, "AAAA"},
+	};
+
+	for (int i = 0; i < 5; i++)
+	{
+		ck_assert_int_eq(strcmp(int_to_dns_type(data[i].type), data[i].result), 0);
+	}
+}
+END_TEST
+
 Suite *dns_resolv_suite(void)
 {
 	Suite *s;
 	TCase *tc_core;
 
-	s = suite_create("dns_resolv");
+	s = suite_create("types");
 	tc_core = tcase_create("Core");
 
 	tcase_add_test(tc_core, test_dns_type_to_int);
+	tcase_add_test(tc_core, test_int_to_dns_type);
+
 	suite_add_tcase(s, tc_core);
 
 	return s;
