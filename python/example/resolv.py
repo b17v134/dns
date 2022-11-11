552ffb723321596c2f25d3305800699255bea390
b17v134 <b17v134@users.noreply.github.com>
2022-11-11 13:48:16 +0200

    Added dns type

--- python/example/resolv.py
+++ python/example/resolv.py
@@ -5,7 +5,7 @@ request.Address = "10.0.2.3"
 request.Port = 53
 request.Protocol = 1
 request.Qname = "github.com"
-request.Type = 1
+request.Type = dns.DNS_TYPE_A
 
 response = dns.Response()
 result = dns.Resolv(request, response)
