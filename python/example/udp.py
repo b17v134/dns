6815df3172275100e7592696b292ae3414ea2832
b17v134 <b17v134@users.noreply.github.com>
2023-02-11 06:10:10 +0000

    Update python package

--- python/example/udp.py
+++ python/example/udp.py
@@ -3,7 +3,7 @@ import dns
 request = dns.Request()
 request.Address = "10.0.2.3"
 request.Port = 53
-request.Protocol = 1
+request.Protocol = dns.Protocol.UDP
 request.Qname = "github.com"
 request.Type = dns.DNS_TYPE_A
 
