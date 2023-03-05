dabc340d025bd3f8450ed64401656a3aacee7a8f
b17v134 <b17v134@users.noreply.github.com>
2023-03-05 07:33:25 +0000

    Updated tests

--- tests/check_types.c
+++ tests/check_types.c
@@ -4,8 +4,6 @@
 
 #include "types.h"
 
-Suite* write_suite(void);
-
 START_TEST(test_dns_type_to_int)
 {
     struct test_data {
@@ -68,8 +66,7 @@ int main(void)
     SRunner* sr;
 
     sr = srunner_create(dns_resolv_suite());
-    srunner_add_suite(sr, write_suite());
-    srunner_run_all(sr, CK_NORMAL);
+    srunner_run_all(sr, CK_VERBOSE);
 
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
