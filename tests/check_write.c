9a96b0838d0c22a86060d399a88ee48d7f182bbb
b17v134 <115415093+b17v134@users.noreply.github.com>
2023-03-06 07:58:46 +0200

    Updated tests (#19)

--- tests/check_write.c
+++ tests/check_write.c
@@ -45,3 +45,16 @@ Suite* write_suite(void)
 
     return s;
 }
+
+int main(void)
+{
+    int number_failed;
+    SRunner* sr;
+
+    sr = srunner_create(write_suite());
+    srunner_run_all(sr, CK_VERBOSE);
+
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
