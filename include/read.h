ac04540a1c12dcead71f1b38b8687eb99a62afd8
b17v134 <b17v134@users.noreply.github.com>
2023-04-01 15:38:27 +0000

    update read qname and update read soa record

--- include/read.h
+++ include/read.h
@@ -5,15 +5,15 @@
 
 #include <stdint.h>
 
-uint8_t read_uint8_t(uint8_t* buf);
-uint16_t read_uint16_t(uint8_t* buf);
-uint32_t read_uint32_t(uint8_t* buf);
-void read_ipv4(uint8_t* buf, char* ip);
-void read_ipv6(uint8_t* buf, char* ip);
-void read_header(uint8_t* buffer, struct header* hdr);
-int read_question(uint8_t* buffer, const int pos, struct question* q);
-int read_qname(uint8_t* buffer, const int pos, char* qname);
-int read_resource_record(uint8_t* buffer, const int pos, struct resource_record* rr);
-void read_soa(uint8_t* buf, const int pos, char* rdata);
+uint8_t read_uint8_t(const uint8_t* buf);
+uint16_t read_uint16_t(const uint8_t* buf);
+uint32_t read_uint32_t(const uint8_t* buf);
+void read_ipv4(const uint8_t* buf, char* ip);
+void read_ipv6(const uint8_t* buf, char* ip);
+void read_header(const uint8_t* buffer, struct header* hdr);
+int read_question(const uint8_t* buffer, const int pos, struct question* q);
+int read_qname(const uint8_t* buffer, const int pos, char* qname);
+int read_resource_record(const uint8_t* buffer, const int pos, struct resource_record* rr);
+void read_soa(const uint8_t* buf, const int pos, char* rdata);
 
 #endif
