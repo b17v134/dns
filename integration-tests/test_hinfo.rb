ec6beceb82151865cfac37a092a611e53ea8c068
b17v134 <b17v134@users.noreply.github.com>
2023-04-21 10:31:35 +0000

    Update test name

--- integration-tests/test_hinfo.rb
+++ integration-tests/test_hinfo.rb
@@ -3,7 +3,7 @@ require 'open3'
 require "test/unit"
 
 class TestHinfo < Test::Unit::TestCase
-    def test_a
+    def test_hinfo
         stdout, stderr, status = Open3.capture3('./integration-tests-dns/bin/resolv -s 10.23.0.7 example.test-bind -t hinfo -o json')
  
         assert_equal(0, status)
